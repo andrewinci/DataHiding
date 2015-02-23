3c28393bcd5ed7d299eb5064091f980998fbaa52
darka91 <andrewinci@gmail.com>
2015-02-23 18:23:52 +0100

    Update compare.m

--- Code/compare.m
+++ Code/compare.m
@@ -29,11 +29,7 @@ points2 = detectHarrisFeatures(I2);
 [features2, valid_points2] = extractFeatures(I2, points2);
 
  indexPairs = matchFeatures(features1, features2);
-
- matchedPoints1 = valid_points1(indexPairs(:, 1), :);
- matchedPoints2 = valid_points2(indexPairs(:, 2), :);
- matchedPoints1.Count
- matchedPoints2.Count
+ disp(indexPairs);
 % figure; showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
 end
 end
