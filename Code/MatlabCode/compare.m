f4bea4fa77f443e8ba7a38a01f7548e2629782dc
darka91 <andrewinci@gmail.com>
2015-02-28 09:52:13 +0100

    fixed for new database

--- Code/MatlabCode/compare.m
+++ Code/MatlabCode/compare.m
@@ -1,23 +1,15 @@
 %import from database 
 addpath ~/Dev/matlab-sqlite3-driver/
 sqlite3.open('/Users/darka/Dev/DataHiding/Code/cache/articles.db');
-base_images = sqlite3.execute('select * from image where article_id = 0');
-articles = sqlite3.execute('select * from article');
-correlated_images = sqlite3.execute('select * from image');
-texts = sqlite3.execute('select * from body');
-
-base_images_n = size(base_images);
-base_images_n = base_images_n(2);
-
-correlated_images_n = size(correlated_images);
-correlated_images_n = correlated_images_n(2);
-
+corr = sqlite3.execute('select * from comparated_image;');
+corr_size = size(corr);
+corr_size = corr_size(2);
+for i=1:corr_size
+  id_base = corr(i).img_base_id;
+  id_corr = corr(i).img_corr_id;
+  img_base_path = corr(i).img_base_path;
+  img_correlated_path = corr(i).img_corr_path;
 %compare
-for i=1:base_images_n
-for j=1:correlated_images_n
-
-img_base_path = base_images(i).local_path;
-img_correlated_path = correlated_images(j).local_path;
 try
 img_base = imread(strcat('/Users/darka/Dev/DataHiding/Code/',img_base_path));
 img_corr = imread(strcat('/Users/darka/Dev/DataHiding/Code/',img_correlated_path));
@@ -30,6 +22,5 @@ C = Correlation(img_base_gray, img_corr_gray);
 disp([H,S,C]);
 catch
     disp('error in imread or in algorithms');
-end
 end
 end
