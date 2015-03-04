e3a4f996fbc3e272d694981075a3b82e3fbfbdef
Carlo Brunetta <brunocarletta@gmail.com>
2015-03-04 23:11:15 +0100

    Comments
    LuL

--- Test/HumanTest/plotter.py
+++ Test/HumanTest/plotter.py
@@ -7,50 +7,43 @@ sim = "select count(*) from comparated_image where is_similar = 1;"
 
 db = sqlite3.connect("Link3.db")
 
-
-
-sh = """select image1.url as 'Base', image2.url as 'Simi' ,comparated_image.SURF,comparated_image.correlation,comparated_image.is_similar from comparated_image
+sh = db.execute("""
+	select image1.url as 'Base', image2.url as 'Simi' ,comparated_image.SURF,comparated_image.correlation,comparated_image.is_similar from comparated_image
 	inner join image as image1 on image1.id = comparated_image.img_base_id
 	inner join image as image2 on image2.id = comparated_image.img_corr_id
 	where comparated_image.img_base_path != comparated_image.img_corr_path
 	and comparated_image.is_similar =1;"""
+	).fetchall()
 
-sh = db.execute(sh).fetchall()
 
-st = ""
-for x in sh:
-	st += "<img src=\""+str(x[0])+"\" width=\"400\" height=\"400\">  - <img src=\""+x[1]+"\" width=\"400\" height=\"400\">  ::: "+ str(x[2]) + " -" + str(x[3]) + " <br/><br/> \n"
+# Commento
 
 print
-print st
+print '\n'.join(["<img src=\""+x[0]+"\" width=\"400\" height=\"400\">  - <img src=\""+x[1]+"\" width=\"400\" height=\"400\">  ::: "+ str(x[2]) + " -" + str(x[3]) + " <br/><br/> " for x in sh])
 print
 
 
-kk = "select SURF,correlation,is_similar from comparated_image where img_base_path != img_corr_path;"
+# kk = "select SURF,correlation,is_similar from comparated_image where img_base_path != img_corr_path;"
 
-kk = db.execute(kk).fetchall()
+kk = db.execute("""
+	select SURF,correlation,is_similar
+	from comparated_image
+	where img_base_path != img_corr_path;"""
+	).fetchall()
 
-xi = yi = xs = ys = []
+# Variabili da plottare
 
-for x in kk:
-	if x[2] == 0:
-		xi = xi + [x[0]]
-		yi = yi + [x[1]]
-	else:
-		xs = xs + [x[0]]
-		ys = ys + [x[1]]
+xi = [ x[2]==0 and x[0]  for x in kk]
+yi = [ x[2]==0 and x[1]  for x in kk]
+xs = [ x[2]==1 and x[0]  for x in kk]
+ys = [ x[2]==1 and x[1]  for x in kk]
+
+# Plotting
 
 plt.xlabel('SURF')
 plt.ylabel('Correlation')
-
 plt.plot(xi, yi, marker='o', linestyle='None', color='b')
 plt.plot(xs, ys, marker='o', linestyle='None', color='r')
-
 plt.show()
-
-
-
-
-
 
 db.close()
