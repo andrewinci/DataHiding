97283139fa4a883314f9d05adc613d39dbd1cc6b
Darka <darka@Darka.local>
2015-03-17 17:19:53 +0100

    Add test with report and db
    +Improve Comparation (todo : write better)

--- Code/DHLib/Chooser.py
+++ Code/DHLib/Chooser.py
@@ -1,13 +1,16 @@
-#!/usr/local/bin/python3n
+#!/usr/local/bin/python3
 import sqlite3
 
+def devstand(a,b,c):
+    re = pow((pow(a-b,2)+pow(a-c,2)+pow(b-c,2))/3,0.5)
+    return re
 def computesimilarity(db):
     sh = db.execute('select SURFmin , SURFmax, correlation, is_similar, img_base_id, img_corr_id from comparated_image').fetchall();
     for x in sh:
         #variable result from query
-        minSurf = x[0]
-        maxSurf = x[1]
-        correlation = x[2]
+        minSurf = x[0] if x[0] is not None else 0
+        maxSurf = x[1] if x[1] is not None else 0
+        correlation = x[2] if x[2] is not None else 0
         is_sim = str(x[3])
         img_base_id = x[4]
         img_corr_id = x[5]
@@ -29,13 +32,17 @@ def computesimilarity(db):
             #no common face
             is_sim ='0'
             info = "face detection"
+
         if is_sim == '2':
             is_sim='0'
             info = 'not sure'
-            if( maxSurf>0 and minSurf>0 and correlation+maxSurf+minSurf>0.5):
-                is_sim='1'
-                info = 'c+Ms+ms>0.5'
 
+        if maxSurf < 0.01 and minSurf < 0.01:
+            is_sim ='0'
+            info ='surf are 0'
+        elif (maxSurf+minSurf+correlation)/3>0.21 and devstand(maxSurf, minSurf, correlation)<0.2:
+            is_sim ='1'
+            info = 'devstand'
         #update database
         db.execute('''update comparated_image set is_similar = ?, info = ?
         where img_base_id = ? and img_corr_id = ?''',
@@ -43,5 +50,10 @@ def computesimilarity(db):
     db.commit()
 
 if __name__ == '__main__':
-    db = sqlite3.connect('../cache/articles.db')
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', help = 'database path')
+    args = parser.parse_args()
+    db_path = args.path if not args.path == None else '../cache/articles.db'
+    db = sqlite3.connect(db_path)
     computesimilarity(db)
