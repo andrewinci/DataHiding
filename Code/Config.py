ce76af601f0ac49289ce47b32c496c7a189cd8ab
Darka <darka@Darka.local>
2015-06-17 17:27:11 +0200

    Add external site for parsing

--- Code/Config.py
+++ Code/Config.py
@@ -34,10 +34,10 @@ ARTICLE_URL = 'http://www.bbc.com/news/world-europe-30708237'
 
 
 #number of article to be downloaded by title (multiplo di 10)
-N_TITLE = 150
+N_TITLE = 10
 
 #number of article to be downloaded by tags (multiplo di 10)
-N_TAG = 150
+N_TAG = 10
 
 #min image size in byte to be consider
 MIN_IMAGE_SIZE = 20000
